1307d31edfbc1285e4aeff70abf80aba362265ec
winny <winny@lavabit.com>
2013-07-09 19:47:51 +0100

    Added simple Minecraft Query protocol capability.

--- mcstat.php
+++ mcstat.php
@@ -1,10 +1,14 @@
 <?php
 
+// This file is in the public domain.
+
 /*
+  ================
+  Server List Ping
+  ================
+
   An example of how to get a Minecraft server status's using a "Server List Ping" packet.
   See details here: http://www.wiki.vg/Server_List_Ping
-
-  This file is in the public domain.
 */
 
 function MC_packString($string)
@@ -53,6 +57,81 @@ function MC_serverListPing($hostname, $port=25565)
                  'server_version' => MC_decodeUCS2BE($response[2]),
                  'protocol_version' => MC_decodeUCS2BE($response[1]),
                  'latency' => $time
+                 );
+}
+
+/*
+  =====
+  Query
+  =====
+
+  This section utilizes the UT3 Query protocol to query a Minecraft server.
+  Read about it here: http://wiki.vg/Query
+*/
+
+function MC_getStrings($fp, $count)
+{
+    $nulsProcessed = 0;
+
+    while ($nulsProcessed < $count) {
+        while ($c != chr(0)) {
+            $s .= $c;
+            $c = fread($fp, 1);
+        }
+
+        $strings[] = $s;
+        $nulsProcessed++;
+
+        unset($c);
+        unset($s);
+    }
+
+    return $strings;
+}
+
+function MC_query($hostname, $port=25565)
+{
+    $sessionId = rand(1, 0xFFFFFFFF) & 0x0F0F0F0F;
+    $handshakeRequest = pack('cccN', 0xFE, 0xFD, 9, $sessionId);
+
+    $fp = stream_socket_client('udp://' . $hostname . ':' . $port);
+    if (!$fp) {
+        return false;
+    }
+
+    fwrite($fp, $handshakeRequest);
+    $handshakeResponse = fread($fp, 2048);
+
+    // Ensure packet type is "handshake" and references our session ID.
+    if (strpos($handshakeResponse, 9) !== 0 && substr(1, 4) != $sessionId) {
+        fclose($fp);
+        error_log('Received invalid handshake response "' . bin2hex($handshakeResponse) . '". Returning.');
+        return false;
+    }
+
+    $challengeToken = substr($handshakeResponse, 5, -1);
+
+    $statRequest = pack('cccNN', 0xFE, 0xFD, 0, $sessionId, $challengeToken);
+    fwrite($fp, $statRequest);
+    $statResponseHeader = fread($fp, 5);
+
+    if (strpos($statResponseHeader, 0) !== 0 && substr($statResponseHeader, 1, 4) != $sessionId) {
+        fclose($fp);
+        error_log('Received invalid stat response header "' . bin2hex($statResponseHeader) . '". Returning.');
+        return false;
+    }
+
+    $statData = array_merge(MC_getStrings($fp, 5), unpack('v', fread($fp, 2)), MC_getStrings($fp, 1));
+
+    fclose($fp);
+    return array(
+                 'motd' => $statData[0],
+                 'gametype' => $statData[1],
+                 'map' => $statData[2],
+                 'players' => $statData[3],
+                 'players_max' => $statData[4],
+                 'port' => $statData[5],
+                 'ip' => $statData[6]
                  );
 }
 
