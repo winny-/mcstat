93b18a8755fff907033d96ae561f962c31c85cec
Winston Weinert <winston@ml1.net>
2014-02-26 13:45:32 -0600

    Query test works on 1.7.x servers. Test different versions at once.
    - Ignore test/server*
    - testrunner.sh tests a few different server versions.
    - server.properties sets query.port since 1.7.x doesn't derive
      the port from the tcp server port.

--- test/testrunner.sh
+++ test/testrunner.sh
@@ -1,14 +1,46 @@
 #!/usr/bin/env bash
-Version='1.7.4'
+Versions='1.5.2 1.7.4 1.7.5'
 Port='9876'
 Hostname='127.0.0.1'
-JarFile="minecraft_server.$Version.jar"
-JarDownload="https://s3.amazonaws.com/Minecraft.Download/versions/$Version/$JarFile"
-SERVERDIR='server'
+
 ServerProperties='test-server.properties'
 
+runtest() {
+    Version=$1
+    JarFile="minecraft_server.$Version.jar"
+    JarDownload="https://s3.amazonaws.com/Minecraft.Download/versions/$Version/$JarFile"
+    ServerDir="server-$Version"
+    run mkdir -p "$ServerDir"
+    killserver
+    if [ ! -e "$ServerDir/$JarFile" ]; then
+        printf 'Downloading %s\n' "$JarFile"
+        run curl -\# -o "$ServerDir/$JarFile" "$JarDownload"
+    fi
+    printf 'Starting server %s ...\n' "$Version"
+    run cp "$ServerProperties" "$ServerDir/server.properties"
+    run sed -e "s|VERSION|$Version|" -e "s|PORT|$Port|" -e "s|MOTD|$Motd|" \
+        -e "s|HOSTNAME|$Hostname|" < config-template.php > config.php
+    cd "$ServerDir"
+    java -jar "$JarFile" -Xmx256M -Xms128M nogui &> "mylog.txt" &
+    cd - >/dev/null
+    echo "$!" > "$ServerDir/PIDFILE"
+
+    # Ideally this script should watch the log. Works for now.
+    echo 'Waiting 20 seconds for sever to initialize.'
+    sleep 20
+
+    echo 'Running tests.'
+    echo
+    phpunit -v --color --debug test.php
+    ret=$?
+
+    killserver
+}
+
 killserver() {
-    kill -TERM $(cat PIDFILE) 2>/dev/null
+    kill -TERM $(cat "$ServerDir/PIDFILE") 2>/dev/null
+    # Same as above comment, should watch for the even, not simply wait a while.
+    sleep 5
 }
 
 run() {
@@ -18,28 +50,9 @@ run() {
     fi
 }
 
-run mkdir -p "$SERVERDIR"
-run cd "$SERVERDIR"
-if [ ! -e "$JarFile" ]; then
-    printf 'Downloading %s\n' "$JarFile"
-    run curl -\# -O "$JarDownload"
-fi
-
-killserver
-
-echo 'Starting server...'
-run cp "../$ServerProperties" 'server.properties'
-sed -e "s|VERSION|$Version|" -e "s|PORT|$Port|" -e "s|MOTD|$Motd|" -e "s|HOSTNAME|$Hostname|" < ../config-template.php > ../config.php
-java -jar "$JarFile" -Xmx256M -Xms128M nogui &>/dev/null &
-echo "$!" > PIDFILE
-
-echo 'Waiting 10 seconds for sever to initialize.'
-sleep 10
-
-echo 'Running tests.'
-echo
-phpunit ../test.php
-ret=$?
-
-killserver
-exit $ret
+errors=0
+for v in $Versions; do
+    runtest "$v"
+    errors=$(($errors + $ret))
+done
+exit $errors
