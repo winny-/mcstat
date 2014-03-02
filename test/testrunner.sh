06b0e667723a7c319ac8bfca5829133fc13aad9d
Winston Weinert <winston@ml1.net>
2014-03-01 23:10:34 -0600

    Colorize testrunner.sh output. Make script more readable.

--- test/testrunner.sh
+++ test/testrunner.sh
@@ -9,20 +9,44 @@ max_players.value 20'
 
 ServerProperties='test-server.properties'
 
-waitForServerStart() {
-    printf 'Waiting for server to start up... '
-    run fgrep -m 1 'Query running on' < "$ServerDir/log.fifo" &>/dev/null
-    printf 'ready.\n'
+ok() {
+    tput setaf 2
+    printf 'OK\n'
+    tput sgr0
 }
 
-waitForServerStop() {
-    printf 'Waiting for server to stop... '
+fail() {
+    tput setaf 1
+    printf 'FAIL'
+    tput sgr0
+}
+
+configureServer() {
+    run cp "$ServerProperties" "$ServerDir/server.properties"
+    run sed -e "s|VERSION|$Version|" -e "s|PORT|$Port|" -e "s|MOTD|$Motd|" \
+        -e "s|HOSTNAME|$Hostname|" < config-template.php > config.php
+    [ ! -p "$ServerDir/log.fifo" ] && run mkfifo "$ServerDir/log.fifo"
+}
+
+startServer() {
+    printf '>> Starting server v%s ... ' "$Version"
+
+    cd "$ServerDir"
+    java -jar "$JarFile" -Xmx256M -Xms128M nogui &> 'log.fifo' &
+    cd - >/dev/null
+    echo "$!" > "$ServerDir/PIDFILE"
+    run fgrep -m 1 'Query running on' < "$ServerDir/log.fifo" &>/dev/null
+    ok
+}
+
+stopServer() {
+    printf '>> Stopping server v%s ... ' "$Version"
     pid=$(<"$ServerDir/PIDFILE")
     while ps "$pid" &>/dev/null; do
         kill "$pid"
         sleep 2
     done
-    printf 'stopped.\n'
+    ok
     rm "$ServerDir/PIDFILE"
 }
 
@@ -34,42 +58,36 @@ runTest() {
     run mkdir -p "$ServerDir"
     [ -e "$ServerDir/PIDFILE" ] && kill $(<"$ServerDir/PIDFILE") &>/dev/null
     if [ ! -e "$ServerDir/$JarFile" ]; then
-        printf 'Downloading %s\n' "$JarFile"
+        printf '>> Downloading %s\n' "$JarFile"
         run curl -\# -o "$ServerDir/$JarFile" "$JarDownload"
     fi
-    printf 'Starting server %s ...\n' "$Version"
-    run cp "$ServerProperties" "$ServerDir/server.properties"
-    run sed -e "s|VERSION|$Version|" -e "s|PORT|$Port|" -e "s|MOTD|$Motd|" \
-        -e "s|HOSTNAME|$Hostname|" < config-template.php > config.php
-    [ ! -p "$ServerDir/log.fifo" ] && run mkfifo "$ServerDir/log.fifo"
-    cd "$ServerDir"
-    java -jar "$JarFile" -Xmx256M -Xms128M nogui &> 'log.fifo' &
-    cd - >/dev/null
-    echo "$!" > "$ServerDir/PIDFILE"
 
-    waitForServerStart
+    printf '> Testing against Minecraft Server v%s\n' "$Version"
 
-    echo 'Running tests.'
-    echo
-    phpunit -v --color --debug test.php
+    configureServer
+    startServer
+
+    echo '>>> Running PHPUnit ...'
+    phpunit --colors test.php
     ret=$?
 
-    echo 'Testing minecraft_users.php against empty server.'
-    reply="$(host="$Hostname" port="$Port" ../minecraft_users.php)"
+    printf '>>> Testing minecraft_users.php against empty server ... '
+    reply="$(host="$Hostname" port="$Port" ../minecraft_users.php 2>/dev/null)"
     if [ "$reply" != "$MinecraftUsersEmpty" ]; then
-        echo 'minecraft_users.php failed "Empty server test".'
-        printf 'Got "%s"\n' "$reply"
+        fail
+        printf '>>> Got "%s"\n' "$reply"
         ret=$(($ret + 1))
     else
-        echo .
+        ok
     fi
 
-    waitForServerStop
+    stopServer
+    echo '===================='
 }
 
 run() {
     if ! "$@"; then
-        printf 'Command "%s" failed. Exiting.\n' "$*" >&2
+        printf '> Command "%s" failed. Exiting.\n' "$*" >&2
         exit 1
     fi
 }
@@ -80,24 +98,33 @@ for v in $Versions; do
     errors=$(($errors + $ret))
 done
 
-echo 'Testing minecraft_users.php against a dead server.'
-reply="$(host="$Hostname" port="$Port" ../minecraft_users.php)"
+printf '>>> Testing minecraft_users.php against a dead server ... '
+reply="$(host="$Hostname" port="$Port" ../minecraft_users.php 2>/dev/null)"
 if [ "$reply" != "$MinecraftUsersDead" ]; then
-    echo 'FAILED'
+    fail
     errors=$(($errors + 1))
 else
-    echo .
+    ok
 fi
-echo 'Testing minecraft_users.php against unresponsive server.'
+printf '>>> Testing minecraft_users.php against unresponsive server ... '
 nc -l "$Port" >/dev/null &
 ncpid=$!
-reply="$(host="$Hostname" port="$Port" ../minecraft_users.php)"
+reply="$(host="$Hostname" port="$Port" ../minecraft_users.php 2>/dev/null)"
 if [ "$reply" != "$MinecraftUsersDead" ]; then
-    echo 'FAILED'
+    fail
     errors=$(($errors + 1))
 else
-    echo .
+    ok
 fi
 kill $ncpid &>/dev/null
 
+if [ $errors -eq 0 ]; then
+    tput setaf 2
+    printf 'All tests passed!\n'
+    tput sgr0
+else
+    tput setaf 1
+    printf '%s tests failed!\n' "$errors"
+    tput sgr0
+fi
 exit $errors
